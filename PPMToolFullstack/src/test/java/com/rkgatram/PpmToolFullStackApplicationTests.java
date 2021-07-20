33a9efdda89b34876759c54cb3afa7ec2e60e829
Ravi Kumar Gatram <ravikumar.g@hcl.com>
2021-07-20 12:30:23 +0530

    My first fulstack project init commit

---
+++ PPMToolFullstack/src/test/java/com/rkgatram/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkgatram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
